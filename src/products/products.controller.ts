e714d0961de4fa3951c5bcaded109021ae2c70a7
Joel Cruz <jdavidhc1710@gmail.com>
2025-05-08 14:04:23 +0200

    Change transport from TCP to NATS

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,36 +11,36 @@ export class ProductsController {
   }
 
   // @Post()
-  @MessagePattern({ cmd: 'create_product' })
+  @MessagePattern('product.create')
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   // @Get()
-  @MessagePattern({ cmd: 'find_all' })
+  @MessagePattern('product.find_all')
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
   // @Get(':id')
-  @MessagePattern({ cmd: 'find_product_by_id' })
+  @MessagePattern('product.find_by_id')
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
   // @Patch(':id')
-  @MessagePattern({ cmd: 'update_product' })
+  @MessagePattern('product.update')
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
   // @Delete(':id')
-  @MessagePattern({ cmd: 'delete_product' })
+  @MessagePattern('product.delete')
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
 
-  @MessagePattern({ cmd: 'validate_product' })
+  @MessagePattern('product.validate')
   validateProduct(@Payload() ids: number[]) {
     return this.productsService.validateProducts(ids);
   }
